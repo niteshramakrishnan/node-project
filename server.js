03c6f7c0bdbf9dfd9ccb2d5fe763fc72b9be41b0
Nitesh Ramakrishnan <nitesh.ramakrishnan@gmail.com>
2020-12-28 23:26:46 -0600

    chapter1

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-//This is a sample code
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname, './static')));
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server running on port ' + port);
+});
