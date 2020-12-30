c4a920897adb36b3064aaea243a10c005db2394c
Nitesh Ramakrishnan <nitesh.ramakrishnan@gmail.com>
2020-12-29 23:50:06 -0600

    chapter2

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.get('/speakers', (request, response) => {
