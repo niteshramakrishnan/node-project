5e9931249747821003305b2da14e59543f01fdb5
Nitesh Ramakrishnan <nitesh.ramakrishnan@gmail.com>
2021-01-09 23:01:23 -0600

    chapter6

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
 const express = require('express');
-const router = express.Router();
 
 const speakersRoute = require('./speakers');
 const feedbackRoute = require('./feedback');
 
-module.exports = (params) => {
-  const { speakerService } = params;
+const router = express.Router();
 
-  router.get('/', async (request, response) => {
-    const artwork = await speakerService.getAllArtwork();
-    const topSpeakers = await speakerService.getList();
-    response.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers, artwork });
+module.exports = params => {
+  const { speakersService } = params;
+
+  router.get('/', async (request, response, next) => {
+    try {
+      const artwork = await speakersService.getAllArtwork();
+      const topSpeakers = await speakersService.getList();
+      return response.render('layout', {
+        pageTitle: 'Welcome',
+        template: 'index',
+        topSpeakers,
+        artwork,
+      });
+    } catch (err) {
+      return next(err);
+    }
   });
 
   router.use('/speakers', speakersRoute(params));
   router.use('/feedback', feedbackRoute(params));
+
   return router;
 };
--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+const createError = require('http-errors');
+
+const bodyParser = require('body-parser');
 
 const FeedbackService = require('./services/FeedbackService');
-const SpeakerService = require('./services/SpeakerService');
+const SpeakersService = require('./services/SpeakerService');
 
 const feedbackService = new FeedbackService('./data/feedback.json');
-const speakerService = new SpeakerService('./data/speakers.json');
+const speakersService = new SpeakersService('./data/speakers.json');
 
 const routes = require('./routes');
-const { nextTick } = require('process');
 
 const app = express();
+
+app.locals.siteName = 'ROUX Academy';
+
 const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
@@ -22,17 +39,35 @@ app.use(express.static(path.join(__dirname, './static')));
 
 app.use(async (request, response, next) => {
   try {
-    const names = await speakerService.getNames();
+    const names = await speakersService.getNames();
     response.locals.speakerNames = names;
-
     return next();
   } catch (err) {
-    return next();
+    return next(err);
   }
 });
 
-app.use('/', routes({ feedbackService, speakerService }));
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
+
+app.use((request, response, next) => {
+  return next(createError(404, 'File not found'));
+});
+
+app.use((err, request, response, next) => {
+  response.locals.message = err.message;
+  console.error(err);
+  const status = err.status || 500;
+  response.locals.status = status;
+  response.status(status);
+  response.render('error');
+});
 
 app.listen(port, () => {
-  console.log('Server running on port ' + port);
+  console.log(`Express server listening on port ${port}!`);
 });
